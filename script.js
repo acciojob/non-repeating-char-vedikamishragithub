e9ca21f5c606dae5e5a07fed7892ade83b26e1e3
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-13 09:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function firstNonRepeatedChar(input) {
+//   // Write your code here
+  
+let frequency = {};
+
+for(let i = 0; i < input.length; i++) {
+    let element = input[i];
+    if(frequency[element]) {
+        frequency[element]++;
+    } else {
+        frequency[element] = 1;
+    }
+
+}
+for(let key in frequency) {
+	if(frequency[key] == 1) {
+		var final = key;
+		
+		break;
+	}
+
+	
+}
+return final;
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
+
